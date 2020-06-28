cf5b73eaba8bc6df8905694bab1d25dcbd3cbb51
Jesse Riggins <jriggins@users.noreply.github.com>
2020-06-28 14:43:17 -0500

    wip

--- app/controllers/todos/create_controller.rb
+++ app/controllers/todos/create_controller.rb
@@ -1,15 +1,31 @@
 module Todos
   class CreateController < ApplicationController
     def create
-      Todo.create!(create_params)
+      create_todo(create_params)
 
       respond_to do |format|
         format.json { render json: {} }
       end
     end
 
+    private
+
     def create_params
       params.permit(:title, :description)
+    end
+
+    def create_todo(params)
+      CreateTodo.new(repository: Todo).call(params: params)
+    end
+  end
+
+  class CreateTodo
+    def initialize(repository:)
+      @repository = repository
+    end
+
+    def call(params:)
+      @repository.create!(params)
     end
   end
 end
